a3c262aca4a1b6b1cd9b5e0e11e5068fa2fe06b3
Alex Johnson <alexmontjohn@gmail.com>
2017-08-23 16:48:05 -0700

    Add showBoard PROC

--- TicTacToe-Assembly/TicTacToe.asm
+++ TicTacToe-Assembly/TicTacToe.asm
@@ -3,12 +3,46 @@
 include Irvine32.inc
     
 .data
-     ; variables here
+board BYTE " 1      |2      |3      ",10,13,
+           "        |       |       ",10,13,
+           "        |       |       ",10,13,
+           " -------+-------+-------",10,13,
+           " 4      |5      |6      ",10,13,
+           "        |       |       ",10,13,
+           "        |       |       ",10,13,
+           " -------+-------+-------",10,13,
+           " 7      |8      |9      ",10,13,
+           "        |       |       ",10,13,
+           "        |       |       ",10,13,0
 
 .code
 main PROC
-     ; code here             
+     push OFFSET board
+     call showBoard
+
+     ; pause before closing window
+     call WaitMsg
 	
      exit
 main ENDP
+
+; Prints board.
+;    Receives: [ebp + 8] = address of board
+;    Returns:  nothing
+showBoard PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; print board
+     mov edx,[ebp + 8]
+     call WriteString
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret  4
+showBoard ENDP
+
 END main
