76a9a51b801b753190183d5f037ec990aaaf6ade
Alex Johnson <alexmontjohn@gmail.com>
2017-08-23 19:25:56 -0700

    Add prototypes for procedures to be implemented

--- TicTacToe-Assembly/TicTacToe.asm
+++ TicTacToe-Assembly/TicTacToe.asm
@@ -6,19 +6,28 @@ include Irvine32.inc
 board BYTE " 1      |2      |3      ",10,13,
            "        |       |       ",10,13,
            "        |       |       ",10,13,
-           " -------+-------+-------",10,13,
+           " -------+-------+-------",10,13, 
            " 4      |5      |6      ",10,13,
            "        |       |       ",10,13,
            "        |       |       ",10,13,
            " -------+-------+-------",10,13,
-           " 7      |8      |9      ",10,13,
+           " 7      |8      |9      ",10,13, 
            "        |       |       ",10,13,
            "        |       |       ",10,13,0
+winsX DWORD 0
+winsO DWORD 0
 
 .code
+printIntro       PROTO
+printBoard       PROTO
+chooseX          PROTO
+chooseO          PROTO
+checkWinner      PROTO
+printOutro       PROTO
+
 main PROC
      push OFFSET board
-     call showBoard
+     call printBoard
 
      ; pause before closing window
      call WaitMsg
@@ -26,10 +35,18 @@ main PROC
      exit
 main ENDP
 
+; Prints intro.
+;    Receives: nothing
+;    Returns:  nothing
+printIntro PROC
+     ; procedure code here
+
+printIntro ENDP
+
 ; Prints board.
 ;    Receives: [ebp + 8] = address of board
 ;    Returns:  nothing
-showBoard PROC
+printBoard PROC
     ; prepare stack frame
      push ebp
      mov  ebp,esp
@@ -43,6 +60,80 @@ showBoard PROC
      popad
      pop  ebp
      ret  4
-showBoard ENDP
+printBoard ENDP
+
+; Prompts player to choose a location on board to place an X. Validates input and
+; will only accept a number between 1 and 9. Cells to place at are as follows:
+; 1 = 30, 2 = 38, 3 = 46, 4 = 134, 5 = 142, 6 = 150, 7 = 238, 8 = 246, 9 = 254
+;    Receives: [ebp + 8] = address of board
+;    Returns:  nothing
+chooseX PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; procedure code here
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret 4
+chooseX ENDP
+
+; Prompts player to choose a location on board to place an O. Validates input and
+; will only accept a number between 1 and 9. Cells to place at are as follows:
+; 1 = 30, 2 = 38, 3 = 46, 4 = 134, 5 = 142, 6 = 150, 7 = 238, 8 = 246, 9 = 254
+;    Receives: [ebp + 8] = address of board
+;    Returns:  nothing
+chooseO PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; procedure code here
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret 4
+chooseO ENDP
+
+; Checks to see if either player has won by looking for runs of 3 X's or O's. This is
+; done by looking at particular cells in the board array. For example, a run in the
+; first row would mean that cells 30, 38, and 46 would all be either X or O.
+;    Receives: [ebp + 8] = address of board  
+;    Returns:  nothing
+checkWinner PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; procedure code here
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret
+checkWinner ENDP
+
+; Prints outro.
+;    Receives: nothing
+;    Returns:  nothing
+printOutro PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; procedure code here
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret
+printOutro ENDP
 
 END main
