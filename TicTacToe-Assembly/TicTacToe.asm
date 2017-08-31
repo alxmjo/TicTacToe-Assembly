7822d36c86d6e43ce938b6fe1685ab071d875063
Alex Johnson <alexmontjohn@gmail.com>
2017-08-31 10:48:47 -0700

    implement range checking in chooseSquare

--- TicTacToe-Assembly/TicTacToe.asm
+++ TicTacToe-Assembly/TicTacToe.asm
@@ -14,18 +14,20 @@ board BYTE " 1      |2      |3      ",10,13,
            " 7      |8      |9      ",10,13, 
            "        |       |       ",10,13,
            "        |       |       ",10,13,0
-playerX BYTE 'X'
-playerY BYTE 'O'
-playerXwins DWORD 0
-playerOwins DWORD 0
-sIntro BYTE "Welcome to TicTacToessembly!",0
-sTurn BYTE "'s turn. Choose a square: ",0
+playerX        BYTE 'X'
+playerY        BYTE 'O'
+currentPlayer  BYTE 'X'
+choice         DWORD ?
+sIntro         BYTE "Welcome to TicTacToessembly!",0
+sTurn          BYTE "'s turn. Choose a square: ",0
 sInvalidChoice BYTE "Invalid choice. Choose a square: ",0
+sWins          BYTE " wins!",0
 
 .code
 printIntro       PROTO
 printBoard       PROTO
-chooseSquare     PROTO   
+chooseSquare     PROTO
+switchPlayer     PROTO
 checkWinner      PROTO
 printOutro       PROTO
 
@@ -38,14 +40,25 @@ Run:
      push OFFSET board
      call printBoard
 
+     mov al,currentPlayer
+     push eax
      push OFFSET board
-     push OFFSET currentPiece
      call chooseSquare
      
+     mov al,currentPlayer
+     push eax
+     call switchPlayer
+     
      ; check if game over
-     jmp Run
+     push OFFSET board
+     call checkWinner
+     cmp eax,0
+     je Run
 
 Finish:
+     ; print outro
+     call printOutro
+     
      ; pause before closing window
      call WaitMsg
 	
@@ -85,47 +98,66 @@ printBoard ENDP
 ; Prompts player to choose a location on board to place an X. Validates input and
 ; will only accept a number between 1 and 9. Cells to place at are as follows:
 ; 1 = 30, 2 = 38, 3 = 46, 4 = 134, 5 = 142, 6 = 150, 7 = 238, 8 = 246, 9 = 254
-;    Receives: [ebp + 8] = address of board
+;    Receives: [ebp +  8] = address of board
+;              [ebp + 12] = currentPlayer
 ;    Returns:  nothing
-chooseX PROC
+chooseSquare PROC
     ; prepare stack frame
      push ebp
      mov  ebp,esp
      pushad
 
-     ; procedure code here
+     ; loop until choice entered
+     mov  eax,[ebp + 12]
+     call WriteChar
+     mov edx,OFFSET sTurn
+     call WriteString
 
+Validate:
+     call ReadDec
+     mov  choice,eax
+     cmp  choice,9
+     ja   Invalid            ; value for terms too high, jump to Invalid
+     cmp  choice,1
+     jb   Invalid            ; value for terms too low, jump to Invalid
+     jmp  Finish
 
+Invalid:
+     mov  edx,OFFSET sInvalidChoice
+     call WriteString
+     jmp  Validate
+
+Finish:
      ; clean up stack frame
      popad
      pop  ebp
-     ret 4
-chooseX ENDP
-
-; Prompts player to choose a location on board to place an O. Validates input and
-; will only accept a number between 1 and 9. Cells to place at are as follows:
-; 1 = 30, 2 = 38, 3 = 46, 4 = 134, 5 = 142, 6 = 150, 7 = 238, 8 = 246, 9 = 254
-;    Receives: [ebp + 8] = address of board
-;    Returns:  nothing
-chooseO PROC
-    ; prepare stack frame
-     push ebp
-     mov  ebp,esp
-     pushad
-
-     ; procedure code here
-
-     ; clean up stack frame
-     popad
-     pop  ebp
-     ret 4
-chooseO ENDP
+     ret 8
+chooseSquare ENDP
 
 ; Checks to see if either player has won by looking for runs of 3 X's or O's. This is
 ; done by looking at particular cells in the board array. For example, a run in the
-; first row would mean that cells 30, 38, and 46 would all be either X or O.
+; first row would mean that cells 30, 38, and 46 would all be either X or O. If winner
 ;    Receives: [ebp + 8] = address of board  
-;    Returns:  nothing
+;    Returns:  0 in EAX if no winner found, 1 in EAX if winner found
+switchPlayer PROC
+    ; prepare stack frame
+     push ebp
+     mov  ebp,esp
+     pushad
+
+     ; procedure code here
+
+     ; clean up stack frame
+     popad
+     pop  ebp
+     ret
+switchPlayer ENDP
+
+; Checks to see if either player has won by looking for runs of 3 X's or O's. This is
+; done by looking at particular cells in the board array. For example, a run in the
+; first row would mean that cells 30, 38, and 46 would all be either X or O. If winner
+;    Receives: [ebp + 8] = address of board  
+;    Returns:  0 in EAX if no winner found, 1 in EAX if winner found
 checkWinner PROC
     ; prepare stack frame
      push ebp
